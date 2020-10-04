b6686c8caa9f5bb87361e332a75d43ddf1d20afb
Olga Poddubnaia <poddubnaiaolga@gmail.com>
2020-10-05 00:29:02 +0300

    connection preferences

---
+++ src/main/java/com/olga/racing/Main.java
@@ -0,0 +1,30 @@
+package com.olga.racing;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class Main {
+    private static final String URL = "jdbc:mysql://localhost:3306/racing_db?useSSL=false&serverTimezone=UTC";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+    public static void main(String[] args) {
+        Connection connection;
+        try {
+            Driver driver = new com.mysql.cj.jdbc.Driver();
+            DriverManager.registerDriver(driver);
+
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            if (!connection.isClosed()) {
+                System.out.println("Соединение с БД установлено");
+            }
+            connection.close();
+
+        } catch (SQLException e) {
+            System.out.println("Ошибка cоединения с БД");
+        }
+    }
+}
